0d892b87f56e138f04448acefb18452c14b4e757
stieludv <19951864+stieludv@users.noreply.github.com>
2024-03-11 16:44:47 +0100

    Contact page header and information

--- src/content/contact.js
+++ src/content/contact.js
@@ -3,8 +3,32 @@ import render from '../functions/renderContent';
 
 const createContact = () => {
     const contactDiv = document.createElement("div");
+    contactDiv.classList.add("contact-div");
 
-    
+    const contactHeader = document.createElement("h1");
+    const contactHeaderText = document.createTextNode("Contact Lorem ipsum");
+    contactHeader.appendChild(contactHeaderText);
+
+    const contactPhone = document.createElement("p");
+    const contactPhoneText = document.createTextNode("+293 23 324 234");
+    contactPhone.appendChild(contactPhoneText);
+
+    const contactEmail = document.createElement("p");
+    const contactEmailText = document.createTextNode("loremipsum@loremipsum.loremupsum");
+    contactEmail.appendChild(contactEmailText);
+
+    const contactAddress = document.createElement("p");
+    const contactAddressText = document.createTextNode("lorem ipsum, ipsum, 234 233");
+    contactAddress.appendChild(contactAddressText);
+
+    // TODO:
+    // Add in a contact form
+    // Perhaps a map/google maps?
+
+    contactDiv.appendChild(contactHeader);
+    contactDiv.appendChild(contactPhone);
+    contactDiv.appendChild(contactEmail);
+    contactDiv.appendChild(contactAddress);
 
     return contactDiv;
 }
